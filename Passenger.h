ee2aca192c73928c44d26a16bb9feb590f48d7fd
psalehi1378 <55737505+psalehi1378@users.noreply.github.com>
2019-09-24 23:00:41 +0800

    Update Passenger.h

--- Passenger.h
+++ Passenger.h
@@ -5,10 +5,12 @@ class Passenger: public User
 {
 private:
 	string name;
+	int PassengerPass = 67890;
 	const string passportNo;
 	const string mileCard;
 
 public:
+	void display();
 	double creditMile();
 	double consumeMiles();
 	void cancelMiles();
