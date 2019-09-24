054dacef75e1964bd2757fa809cb6ebe5b108782
psalehi1378 <55737505+psalehi1378@users.noreply.github.com>
2019-09-24 22:53:36 +0800

    Update Admin.cpp

--- Admin.cpp
+++ Admin.cpp
@@ -11,3 +11,22 @@
 #include "Airline.h"
 #include "FlightHandling.h"
 using namespace std;
+
+void Admin::displayAdmin()
+{
+	system("CLS");
+	cout << "===========================================" << endl;
+	cout << "=\tYou are logging in as an Admin. =\n ";
+	cout << "=\t\tEnter Your Password: ";
+	int password;
+	cin >> password;
+	if (password == AdminPass)
+	{
+		cout << "******** You are Logged in ********\n";
+	}
+	else
+	{
+		cout << "Incorrect Password\n Please Try again\n";
+		displayAdmin();
+	}
+}
