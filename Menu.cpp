bcdadeb9588ec4b2cd7efcd6bf70c9bc8eabf3db
psalehi1378 <55737505+psalehi1378@users.noreply.github.com>
2019-09-24 23:01:59 +0800

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -26,51 +26,14 @@ int Menu::displayMain()
 	cout << "=  //           ///////////        //              =" << endl;
 	cout << "=            A Flight Management System            =" << endl;
 	cout << "====================================================" << endl;
-	cout << std::endl;
-	cout << "||\t\tSelect The Option:        \t\t  ||" << endl;
-	cout << "||\t\t\t(1) Login As Admin        \t  ||" << endl;
-	cout << "||\t\t\t(2) Use As Passenger      \t  ||" << endl;
+	cout << endl;
+	cout << "||\t\t      Select The Option:        \t\t  ||" << endl;
+	cout << "||\t\t\t(1) Login as admin              \t  ||" << endl;
+	cout << "||\t\t\t(2) Login as passenger          \t  ||" << endl;
+	cout << "||\t\t\t(3) Sign Up                     \t  ||" << endl;
 
 	cout << "\n\tEnter Your Choice : ";
 	int choice;
 	cin >> choice;
 	return choice;
-}
-
-void Menu::displayAdmin()
-{
-	system("CLS");
-	cout << "===========================================" << endl;
-	cout << "=\tYou are logging in as an Admin. =\n ";
-	cout << "=\t\tEnter Your Password: ";
-	int password;
-	cin >> password;
-	if (password == admPass)
-	{
-		cout << "******** You are Logged in ********\n";
-	}
-	else 
-	{
-		cout << "Incorrect Password\n Please Try again\n";
-		displayAdmin();
-	}
-}
-
-void Menu::displayPassenger()
-{
-	system("CLS");
-	cout << "===========================================" << endl;
-	cout << "=\tYou are logging in as an Passenger. =\n ";
-	cout << "=\t\tEnter Your Password: ";
-	int password;
-	cin >> password;
-	if (password == PassengerPass)
-	{
-		cout << "******** You are Logged in ********\n";
-	}
-	else
-	{
-		cout << "Incorrect Password\n Please Try again\n";
-		displayPassenger();
-	}
 }
