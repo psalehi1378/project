bcbd65b7c0e118614a06b050e669612482f8dc4b
psalehi1378 <55737505+psalehi1378@users.noreply.github.com>
2019-09-27 20:40:21 +0800

    Update Airport.h

--- Airport.h
+++ Airport.h
@@ -4,12 +4,29 @@ using namespace std;
 class Airport
 {
 private:
-	const string name;
+	string name[5];
 	int code;
-	const string country;
+	string country[5];
 	double fee;
+	void Airportname()
+	{
+		name[0] = "KLIA";
+		name[1] = "HKG";
+		name[2] = "Singapore Changi Airport";
+		name[3] = "IKIA";
+		name[4] = "NRT";
+	}
+	void Countryname()
+	{
+		country[0] = "Malaysia";
+		country[1] = "Hong Kong";
+		country[2] = "Singapore";
+		country[3] = "Iran";
+		country[4] = "Japan";
+	}
 
 public:
+	Airport();
 
 };
 
