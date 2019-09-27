99e3fcd81d6f9b02490905cb9ffdd8c32cc67858
psalehi1378 <55737505+psalehi1378@users.noreply.github.com>
2019-09-27 20:39:59 +0800

    Update FlightHandling.h

--- FlightHandling.h
+++ FlightHandling.h
@@ -4,12 +4,22 @@ using namespace std;
 class FlightHandling
 {
 private:
-	char boardingGate;
+	string boardingGate[6];
 	double Delay;
 	int date;
 	int time;
+	void setGate()
+	{
+		boardingGate[0] = "A1";
+		boardingGate[1] = "A2";
+		boardingGate[2] = "B1";
+		boardingGate[3] = "B2";
+		boardingGate[4] = "C1";
+		boardingGate[5] = "C2";
+	}
 
 public:
+	FlightHandling();
 	void startCheckIn();
 	void checkInPassenger();
 	void stopCheckIn();
